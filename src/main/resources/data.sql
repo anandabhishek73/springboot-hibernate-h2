e1800bd8cd92cc5ab871fd826cadaff684dc81f3
Abhishek Anand <anand.abhishek73@gmail.com>
2020-12-23 18:07:13 +0530

    Bootstrap Data to be inserter upon startup. (need better way for multi-app-instance entity insertions)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+--insert into book values(1, 'The Tartar Steppe');
+insert into author(id, first_name, last_name, DOB) values(1, 'Abhishek', 'Anand', '2020-10-01');
+insert into book(id, name, ISBN, author_id) values(1, 'Poem Strip', 'B1234A', 1);
+insert into book(id, name, ISBN, author_id) values(2, 'Poem Strip Vol 2', 'B122334A', 1);
+--insert into book values(3, 'Restless Nights: Selected Stories of Dino Buzzati');
+--insert into book values(4, 'Restless Nights: Selected Stories of Dino Buzzatiiiii');
